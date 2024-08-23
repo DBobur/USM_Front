d2e2a8e502d91c72d198527356915026875731ba
bobur <davlatboyev2024@gmail.com>
2024-08-22 20:54:50 -0700

    started

---
+++ src/main/java/uz/pro/usm_front/UsmFrontApplication.java
@@ -0,0 +1,13 @@
+package uz.pro.usm_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsmFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsmFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pro/usm_front/UsmFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pro.usm_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsmFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
